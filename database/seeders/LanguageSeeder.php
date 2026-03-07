a32ae07267db0bc4d8051ccaf0e445f76799f124
Hamidullo Rahmonberdiyev <hamidullo0760@gmail.com>
2026-03-07 16:54:59 +0500

    changes saved

--- database/seeders/LanguageSeeder.php
+++ database/seeders/LanguageSeeder.php
@@ -11,9 +11,9 @@ class LanguageSeeder extends Seeder
     {
         // ID: 1 = en, 2 = ru, 3 = uz
         $languages = [
-            1 => ['code' => 'en', 'name' => 'English'],
+            3 => ['code' => 'en', 'name' => 'English'],
             2 => ['code' => 'ru', 'name' => 'Русский'],
-            3 => ['code' => 'uz', 'name' => 'O\'zbekcha'],
+            1 => ['code' => 'uz', 'name' => 'O\'zbekcha'],
         ];
 
         foreach ($languages as $id => $lang) {
